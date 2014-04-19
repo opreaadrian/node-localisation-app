7a2cceb84fb84bd2540bc4c061f1f674c298b959
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-04-20 02:29:05 +0300

    Update route logic

--- routes/gethelp.js
+++ routes/gethelp.js
@@ -1,6 +1,16 @@
 exports.index = function(req, res) {
 
   res.render('get-help', {
-    title: 'Get help'
+    pageTitle: 'Get help',
+    labelText: 'Phone no.',
+    placeholderText: 'Enter your phone no.',
+    buttonText: 'Go!',
+    scripts: [
+      '//maps.googleapis.com/maps/api/js?key=AIzaSyD0nA1W3_fKddzqEUu7StcwMOCVof-oBZ4&sensor=true'
+    ]
   });
+};
+
+exports.send = function(req, res) {
+  var phone = req.body.phoneNumber;
 };
