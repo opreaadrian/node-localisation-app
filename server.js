d83faf8eab2a65bc902b3f7d7b55510e41479ec7
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-04-20 00:37:07 +0300

    Change application port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var express = require('express'),
 
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', ejs);
