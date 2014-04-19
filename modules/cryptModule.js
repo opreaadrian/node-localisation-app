e7dc1427c031dfbe6dd272a43d8952d05ba41943
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-04-20 00:55:36 +0300

    Update documentation for cryptModule

--- modules/cryptModule.js
+++ modules/cryptModule.js
@@ -1,11 +1,25 @@
+/* Require our dependencies*/
 var crypto = require('crypto');
 
+/**
+ * @module Crypt The main constructor for our encrypter
+ * @constructor
+ *
+ * @param {string} key The key to be encrypted
+ */
 var Crypt = function(key) {
   this.key = key;
 };
 
-Crypt.prototype.encode = function() {
 
+/**
+ * @method encode Carries the responsibility of turning the key into an 256b
+ * encrypted AES key
+ *
+ * @return {string} crypt The encrypted version of the key
+ *
+ */
+Crypt.prototype.encode = function() {
   var cipher = crypto.createCipher('aes-256-ecb', 'secret', ''),
       crypt = cipher.update(this.key, 'utf8', 'binary');
 
@@ -14,15 +28,20 @@ Crypt.prototype.encode = function() {
   return crypt;
 };
 
+/**
+ * @method encode Carries the responsibility of turning the key from a  256b
+ * encrypted AES key back to a regular UTF-8 string
+ *
+ * @return {string} crypt The decrypted version of the key
+ *
+ */
 Crypt.prototype.decode = function() {
-
   var decipher = crypto.createDecipher('aes-256-ecb', 'secret', ''),
       crypt = decipher.update(this.key, 'binary', 'utf8');
 
   crypt += decipher.final('utf8');
 
   return crypt;
-
 };
 
 module.exports = Crypt;
