7235388bd6aaa10dd88ee325f09bf52f559090aa
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-04-20 00:37:31 +0300

    Update application landing page logic

--- routes/index.js
+++ routes/index.js
@@ -3,45 +3,21 @@
  * GET home page.
  */
 
-var crypto = require('crypto');
-
-function encode(key) {
-
-  var cipher = crypto.createCipher('aes-256-ecb', 'secret', ''),
-      crypt = cipher.update(key, 'utf8', 'binary');
-
-  crypt += cipher.final('binary');
-
-  return crypt;
-}
-
-function decode(key) {
-
-  var decipher = crypto.createDecipher('aes-256-ecb', 'secret', ''),
-      crypt = decipher.update(key, 'binary', 'utf8');
-
-  crypt += decipher.final('utf8');
-
-  return crypt;
-
-}
+var Crypt = require('../modules/cryptModule.js');
 
 exports.index = function(req, res){
 
   var io = require('socket.io').listen(3000),
       phoneNo = req.params.phoneno,
-
-      encodedKey = encode(phoneNo);
-
-
+      crypter = new Crypt(phoneNo),
+      encodedKey = crypter.encode();
 
   if (encodedKey) {
     res.render(
       'index',
       {
         title: 'Node-Location',
-        hash: encodedKey,
-        phone: decode(encodedKey)
+        hash: encodedKey
       }
     );
   }
