db76179f4a0dc26efb633980cae3c21d0cc34556
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-05-11 20:19:17 +0200

    Abstract socket.io client logic to separate script

---
+++ public/javascripts/realtime.js
@@ -0,0 +1,17 @@
+var socket = io.connect('http://localhost:8080');
+socket.on('connected', function(data) {
+
+    var hasGeo = 'geolocation' in window.navigator;
+
+    function showData(position) {
+      console.log(position);
+      socket.emit('localized', {coords: position.coords});
+    }
+
+    if (hasGeo) {
+
+      navigator.geolocation.getCurrentPosition(showData);
+
+    }
+    console.log(data);
+});
