d03188499a3572454bfbf5f9fa8649b3a57761fd
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-06-16 00:27:52 +0300

    Whitespace updates

--- routes/gethelp.js
+++ routes/gethelp.js
@@ -1,17 +1,17 @@
-var fs          = require('fs'),
-    mapsApiKey  = JSON.parse(fs.readFileSync(__dirname + '/../mapsapiconfig.json')).apiKey,
-    twilio      = JSON.parse(fs.readFileSync( __dirname + '/../twilio.json'));
+var fs         = require('fs'),
+    mapsApiKey = JSON.parse(fs.readFileSync(__dirname + '/../mapsapiconfig.json')).apiKey,
+    twilio     = JSON.parse(fs.readFileSync( __dirname + '/../twilio.json'));
 
 exports.index = function(req, res) {
   'use strict';
 
   res.render('get-help', {
-    pageTitle: 'Get help',
-    noform: false,
-    labelText: 'Phone no.',
-    placeholderText: 'Enter your phone no.',
-    buttonText: 'Go!',
-    scripts: [
+    pageTitle       : 'Get help',
+    noform          : false,
+    labelText       : 'Phone no.',
+    placeholderText : 'Enter your phone no.',
+    buttonText      : 'Go!',
+    scripts         : [
       '//maps.googleapis.com/maps/api/js?key=' + mapsApiKey + '&sensor=true',
       'javascripts/mapinit.js'
     ]
@@ -22,27 +22,25 @@ exports.send = function(req, res) {
   'use strict';
 
   /** Twilio private config */
-    var accountSid    = twilio.sid,
-        fromPhoneNo   = twilio.fromPhoneNo,
-        authToken     = twilio.token,
-        client        = require('twilio')(accountSid, authToken),
-
-    /** User's phone number -- inserted on the help page*/
-    phone = req.body.phoneNumber;
+    var accountSid  = twilio.sid,
+        fromPhoneNo = twilio.fromPhoneNo,
+        authToken   = twilio.token,
+        client      = require('twilio')(accountSid, authToken),
+        phone = req.body.phoneNumber; // User's phone number -- inserted on the help page
 
   client.sms.messages.create({
-    body: 'The phone number from where the request was made: ' + phone,
-    to: phone,
-    from: fromPhoneNo,
+    body : 'The phone number from where the request was made: ' + phone,
+    to   : phone,
+    from : fromPhoneNo,
   }, function(err, message) {
     process.stdout.write(message.sid);
   });
 
   res.render('get-help', {
-    pageTitle: 'Get help',
-    noform: true,
-    message: 'You will be contacted shortly!',
-    scripts: [
+    pageTitle : 'Get help',
+    noform    : true,
+    message   : 'You will be contacted shortly!',
+    scripts   : [
       '//maps.googleapis.com/maps/api/js?key=' + mapsApiKey + '&sensor=true',
       'javascripts/mapinit.js'
     ]
