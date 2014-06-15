e41413191ee71a8b640a22b21ed0dfdb64b78e39
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-06-16 01:39:32 +0300

    Prettify auth configuration

--- config/auth.js
+++ config/auth.js
@@ -1,17 +1,17 @@
 module.exports = {
   facebookAuth : {
-    clientID: 'client-id',
-    clientSecret: 'client-secret',
-    callbackURL : 'http://localhost:8080/auth/facebook/callback'
+    clientID     : 'client-id',
+    clientSecret : 'client-secret',
+    callbackURL  : 'http://localhost:8080/auth/facebook/callback'
   },
   twitterAuth  : {
-    consumerID: 'consumer-id',
-    consumerSecret: 'consumer-secret',
-    callbackURL : 'http://localhost:8080/auth/facebook/callback'
+    consumerID     : 'consumer-id',
+    consumerSecret : 'consumer-secret',
+    callbackURL    : 'http://localhost:8080/auth/facebook/callback'
   },
   googleAuth   : {
-    clientID: 'client-id',
-    clientSecret: 'client-secret',
-    callbackURL : 'http://localhost:8080/auth/facebook/callback'
+    clientID     : 'client-id',
+    clientSecret : 'client-secret',
+    callbackURL  : 'http://localhost:8080/auth/facebook/callback'
   }
 };
