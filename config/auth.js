501e22f13d63f605ceffb4e342298677a87b4797
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-06-16 01:29:56 +0300

    Initial commit for auth.js file -- structure

---
+++ config/auth.js
@@ -0,0 +1,17 @@
+module.exports = {
+  facebookAuth : {
+    clientID: 'client-id',
+    clientSecret: 'client-secret',
+    callbackURL : 'http://localhost:8080/auth/facebook/callback'
+  },
+  twitterAuth  : {
+    consumerID: 'consumer-id',
+    consumerSecret: 'consumer-secret',
+    callbackURL : 'http://localhost:8080/auth/facebook/callback'
+  },
+  googleAuth   : {
+    clientID: 'client-id',
+    clientSecret: 'client-secret',
+    callbackURL : 'http://localhost:8080/auth/facebook/callback'
+  }
+};
