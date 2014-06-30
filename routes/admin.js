bf3b529f713a61cae4a1b9fdc13fd66b58ead757
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-06-30 17:16:08 +0300

    Add adminitrator routes, administrator-related templates, manager-related templates

---
+++ routes/admin.js
@@ -0,0 +1,23 @@
+var fs = require('fs'),
+  mapsApiKey = JSON.parse(fs.readFileSync('./config/mapsapiconfig.json')).apiKey,
+  twilio = JSON.parse(fs.readFileSync('./config/twilio.json'));
+
+/*
+ * GET home page.
+ */
+
+exports.dashboard = function (req, res) {
+  'use strict';
+
+  res.render('admin/dashboard', {
+    pageTitle : 'Administrator Dashboard',
+    session   : req.session,
+    user      : req.user,
+    scripts   : [
+      '//maps.googleapis.com/maps/api/js?key=' + mapsApiKey + '&sensor=true',
+      '/javascripts/mapinit.js',
+      '/components/bootstrap/js/popover.js',
+      '//code.createjs.com/tweenjs-0.5.1.min.js'
+    ]
+  });
+};
