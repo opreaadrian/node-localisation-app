2a9a5712339c77c0affe678c8c2732ac9f3bcd17
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-06-30 17:09:47 +0300

    Separate user model from administrator model

--- models/administrator.js
+++ models/administrator.js
@@ -7,33 +7,11 @@ var mongoose               = require('mongoose'),
     LOCK_TIME              = 2 * 60 * 60 * 1000, // Time in miliseconds (2h)
     reasons                = null;
 
-var UserSchema = new Schema({
+var AdminSchema = new Schema({
 
-  local          : {
-    email        : String,
-    password     : String
-  },
-  facebook       : {
-    id           : String,
-    token        : String,
-    email        : String,
-    name         : String,
-    profileImage : String
-  },
-  twitter        : {
-    id           : String,
-    token        : String,
-    displayName  : String,
-    username     : String,
-    profileImage : String
-  },
-  google         : {
-    id           : String,
-    token        : String,
-    name         : String,
-    email        : String,
-    profileImage : String
-  },
+  email        : String,
+  password     : String,
+
   loginAttempts  : {
     type         : Number,
     required     : true,
@@ -47,18 +25,18 @@ var UserSchema = new Schema({
 });
 
 // generating a hash
-UserSchema.methods.generateHash = function(password) {
+AdminSchema.methods.generateHash = function(password) {
   'use strict';
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 // checking if password is valid
-UserSchema.methods.validPassword = function(password) {
+AdminSchema.methods.validPassword = function(password) {
   'use strict';
-    return bcrypt.compareSync(password, this.local.password);
+    return bcrypt.compareSync(password, this.password);
 };
 
-UserSchema.methods.incrementLoginAttempts = function(fn) {
+AdminSchema.methods.incrementLoginAttempts = function(fn) {
   'use strict';
 
   var updates = null;
@@ -79,13 +57,13 @@ UserSchema.methods.incrementLoginAttempts = function(fn) {
   return this.update(updates, fn);
 };
 
-reasons = UserSchema.statics.failedLogin = {
+reasons = AdminSchema.statics.failedLogin = {
   NOT_FOUND                 : 0,
   PASSWORD_INCORRECT        : 1,
   MAXIMUM_ATTEMPTS_EXCEEDED : 2
 };
 
-UserSchema.statics.getAuthenticationStatus = function(username, password, fn) {
+AdminSchema.statics.getAuthenticationStatus = function(username, password, fn) {
   'use strict';
 
   this.findOne({ username: username }, function(err, user) {
@@ -109,10 +87,10 @@ UserSchema.statics.getAuthenticationStatus = function(username, password, fn) {
   });
 };
 
-UserSchema.virtual('isLocked').get(function() {
+AdminSchema.virtual('isLocked').get(function() {
   'use strict';
 
   return !!(this.lockUntil && this.lockUntil > Date.now());
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('Administrator', AdminSchema);
