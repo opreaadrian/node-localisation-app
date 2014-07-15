69501fff8844321375d2c2a81de22f63884f96d8
Adrian Oprea <opreaadriangeorge@gmail.com>
2014-07-15 04:53:05 +0300

    updates

--- config/auth.js
+++ config/auth.js
@@ -2,16 +2,16 @@ module.exports = {
   facebookAuth : {
     clientID     : '689846884385946',
     clientSecret : '5028148b2bedf7943028b22546005f49',
-    callbackURL  : 'http://www.trackrapp.com:8080/auth/facebook/callback'
+    callbackURL  : 'http://trackrapplication.herokuapp.com/auth/facebook/callback'
   },
   twitterAuth  : {
     consumerKey     : 'AnJAKH5k2jcLahJEQ0VsLeIIm',
     consumerSecret : 'UkvJAHJ82HEQYIrUaoca2BUODWKWoWmjD7dovqZuI24sfkBJxQ',
-    callbackURL    : 'http://www.trackrapp.com:8080/auth/twitter/callback'
+    callbackURL    : 'http://trackrapplication.herokuapp.com/auth/twitter/callback'
   },
   googleAuth   : {
     clientID     : '403477775984-2jckrrmraqq4k2uqqpuakah2vrfccial.apps.googleusercontent.com',
     clientSecret : 'mRsz3Zt-PfLeGgFnli8r8H-b',
-    callbackURL  : 'http://www.trackrapp.com:8080/auth/google/callback'
+    callbackURL  : 'http://trackrapplication.herokuapp.com/auth/google/callback'
   }
 };
