1314dabb25ad400a1d4805ffd6f19d86a11592f4
opreaadrian <opreaadriangeorge@gmail.com>
2014-08-16 00:28:14 +0300

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,17 +1,17 @@
 module.exports = {
   facebookAuth : {
-    clientID     : '689846884385946',
-    clientSecret : '5028148b2bedf7943028b22546005f49',
+    clientID     : 'fbClientID',
+    clientSecret : 'fbClientSecret',
     callbackURL  : 'http://trackrapplication.herokuapp.com/auth/facebook/callback'
   },
   twitterAuth  : {
-    consumerKey     : 'AnJAKH5k2jcLahJEQ0VsLeIIm',
-    consumerSecret : 'UkvJAHJ82HEQYIrUaoca2BUODWKWoWmjD7dovqZuI24sfkBJxQ',
+    consumerKey     : 'twConsumerKey',
+    consumerSecret : 'twConsumerSecret',
     callbackURL    : 'http://trackrapplication.herokuapp.com/auth/twitter/callback'
   },
   googleAuth   : {
-    clientID     : '403477775984-2jckrrmraqq4k2uqqpuakah2vrfccial.apps.googleusercontent.com',
-    clientSecret : 'mRsz3Zt-PfLeGgFnli8r8H-b',
+    clientID     : 'googClientID',
+    clientSecret : 'googClientSecret',
     callbackURL  : 'http://trackrapplication.herokuapp.com/auth/google/callback'
   }
 };
